85b925dcae00d199d0e778d79d2d0e28b0c82fe1
Daniel Mangowi <daniel@.mangowicom>
2018-01-20 12:46:04 +0100

    added json server data onselected event and on active selected row and ngIf and ngFor directives

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-
+import {HEROES} from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,13 +8,20 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm Daniel'
-  }
+ // hero: Hero = {
+ //   id: 1,
+  //  name: 'Windstorm Daniel'
+  //}
+  selectedHero: Hero;
+
+  heroes = HEROES;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelected = (hero: Hero) => {
+      //console.log("hero: " + selectedHero.name, selectedHero.id)
+      this.selectedHero = hero;
+    }
 }
